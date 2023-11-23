633ffc2e00c5cc9fe0588338adcb49f975bfb06c
Destcable <artmobpavlov21@gmail.com>
2023-11-23 11:58:11 +0300

    [upd]

---
+++ src/components/tasks/TaskHeader.vue
@@ -0,0 +1,11 @@
+<template> 
+<div class="p-2">
+<button class="amocrm-btn-primary amocrm-btn-sm">Интеграция</button>
+</div>
+</template>
+
+<script>
+export default{ 
+    name: 'TaskHeader'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,33 @@
 <template>
     <NavbarComponent />
-    <TaskItem />
+    <div class="bg-white m-5">
+        <TaskHeader/>
+        <SeparatorComponent />
+        <TaskItem />
+        <SeparatorComponent />
+        <TaskItem />
+        <SeparatorComponent />
+    </div>
 </template>
 
 <script>
 import NavbarComponent from '../components/common/Navbar.vue'
+import TaskHeader from '../components/tasks/TaskHeader.vue'
 import TaskItem from '../components/tasks/TaskItem.vue'
+import SeparatorComponent from '../components/ui/Separator.vue'
 export default{
     name: 'HomeView',
     components: {
         NavbarComponent,
+        SeparatorComponent,
+        TaskHeader,
         TaskItem
     }
 }
 </script>
+
+<style>
+body{ 
+    background-color: #F5F5F5;
+}
+</style>
